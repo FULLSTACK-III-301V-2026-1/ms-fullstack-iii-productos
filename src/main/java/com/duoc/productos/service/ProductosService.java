f482f834df01d5898dc956700742d0ed6014dddb
viyyy <vi.castillop@duocuc.cl>
2026-04-29 20:55:43 -0400

    Update ProductosService.java
    se agrega codigo crud

--- src/main/java/com/duoc/productos/service/ProductosService.java
+++ src/main/java/com/duoc/productos/service/ProductosService.java
@@ -1,7 +1,36 @@
 package com.duoc.productos.service;
 
+import com.duoc.productos.model.Productos;
+import com.duoc.productos.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductosService {
+
+    @Autowired
+    private ProductoRepository productosRepository;
+
+    public List<Productos> obtenerTodos() {
+        return productosRepository.findAll();
+    }
+
+    public Optional<Productos> obtenerPorId(Integer id) {
+        return productosRepository.findById(id);
+    }
+
+    public Productos guardar(Productos producto) {
+        return productosRepository.save(producto);
+    }
+
+    public void eliminar(Integer id) {
+        productosRepository.deleteById(id);
+    }
+
+    public boolean existePorId(Integer id) {
+        return productosRepository.existsById(id);
+    }
 }
