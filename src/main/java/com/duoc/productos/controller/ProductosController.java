5ebdd5cdc6493967ca1846014abc247c1493cd14
SB- Alumno <SB-Alumno@duoc.acad>
2026-04-29 20:59:11 -0400

    feat: se agrego un POST para guardar productos

--- src/main/java/com/duoc/productos/controller/ProductosController.java
+++ src/main/java/com/duoc/productos/controller/ProductosController.java
@@ -1,9 +1,23 @@
 package com.duoc.productos.controller;
 
+import com.duoc.productos.model.Productos;
+import com.duoc.productos.service.ProductosService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/productos")
 public class ProductosController {
+
+    @Autowired
+    private ProductosService productosService;
+
+    @PostMapping("/productos")
+    public ResponseEntity<Productos> guardar(@RequestBody Productos productos) {
+        return ResponseEntity.ok(productosService.guardar(productos));
+    }
 }
--- src/main/java/com/duoc/productos/service/ProductosService.java
+++ src/main/java/com/duoc/productos/service/ProductosService.java
@@ -1,7 +1,15 @@
 package com.duoc.productos.service;
 
+import com.duoc.productos.model.Productos;
+import com.duoc.productos.repository.ProductoRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductosService {
+
+    private ProductoRepository productoRepository;
+
+    public Productos guardar(Productos productos) {
+        return productoRepository.save(productos);
+    }
 }
