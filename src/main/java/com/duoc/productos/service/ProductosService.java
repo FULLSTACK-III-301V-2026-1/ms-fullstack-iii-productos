81991f98c475ba3df4a4e4982ec9c807b486b0db
marcprado <marc.prado@duocuc.cl>
2026-04-29 20:48:08 -0400

    se crea obtener productos, crear nuevo ṕroducto y eliminar producto

--- src/main/java/com/duoc/productos/service/ProductosService.java
+++ src/main/java/com/duoc/productos/service/ProductosService.java
@@ -1,7 +1,35 @@
 package com.duoc.productos.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.duoc.productos.model.Productos;
+import com.duoc.productos.repository.ProductoRepository;
 
 @Service
 public class ProductosService {
+    @Autowired
+
+    private ProductoRepository productoRepository;
+
+    // Obtener todos los productos
+    public List<Productos> obtenerTodos() {
+        return productoRepository.findAll();
+    }
+
+    // Crear nuevo producto
+    public Productos crearProducto(Productos producto) {
+        return productoRepository.save(producto);
+    }
+
+    // Eliminar producto
+    public boolean eliminarProducto(Long id) {
+        if (productoRepository.existsById(id)) {
+            productoRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
