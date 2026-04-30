eae9c84313613d04379077a19feb45a89a9d58c7
SB- Alumno <SB-Alumno@duoc.acad>
2026-04-29 20:46:25 -0400

    feat: agrega método POST para crear productos en ProductosController

--- src/main/java/com/duoc/productos/controller/ProductosController.java
+++ src/main/java/com/duoc/productos/controller/ProductosController.java
@@ -1,9 +1,25 @@
 package com.duoc.productos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.duoc.productos.model.Productos;
+import com.duoc.productos.repository.ProductoRepository;
 
 @RestController
 @RequestMapping("/api/v1/productos")
 public class ProductosController {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @PostMapping
+    public ResponseEntity<Productos> crearProducto(@RequestBody Productos producto) {
+        Productos productoGuardado = productoRepository.save(producto);
+        return new ResponseEntity<>(productoGuardado, HttpStatus.CREATED);
+    }
 }
